f61b785a8e432cc1eed0e639bc883f4c81fe07f2
Gabriel Moncau (laptop) <gabriel.moncau@hotmail.com>
2018-09-09 10:00:38 -0400

    Testing HTML and CSS to strings

--- Unity Project/DnDSim/Assets/Scripts/CharacterSheetController.cs
+++ Unity Project/DnDSim/Assets/Scripts/CharacterSheetController.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class CharacterSheetController : MonoBehaviour
 {
+    public GameObject textureDisplay;
     void Awake()
     {
         GameController.characterSheetUI = gameObject;
@@ -13,29 +16,32 @@ public class CharacterSheetController : MonoBehaviour
     private void Start()
     {
         StartCoroutine(GetSpells());
+        populateSpells();
     }
 
     public void populateSpells()
     {
-
-    }
+        using (WebClient client = new WebClient()) // WebClient class inherits IDisposable
+        {
+            client.DownloadFile("http://dnd5e.wikia.com/wiki/Warding_Wind", @"C:\Users\gabriel.tubiomoncau\Desktop\localfile.html");
+        }
+   }
 
     IEnumerator GetSpells()
     {
-        UnityWebRequest www = UnityWebRequest.Get("http://dnd5e.wikia.com/wiki/Acid_Splash");
+        UnityWebRequest www = UnityWebRequest.Get("http://dnd5e.wikia.com/wiki/Warding_Wind");
         yield return www.SendWebRequest();
 
         if (www.isNetworkError || www.isHttpError)
         {
-            Debug.Log(www.error);
+            //Debug.Log(www.error);
         }
         else
         {
             // Show results as text
-            Debug.Log(www.downloadHandler.text);
-
-            // Or retrieve results as binary data
-            byte[] results = www.downloadHandler.data;
+            //Debug.Log(www.downloadHandler.text);
         }
+        string str = www.downloadHandler.text.Substring(www.downloadHandler.text.IndexOf("Self"));
+        Debug.Log(str);
     }
 }
