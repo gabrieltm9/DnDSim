248b8e96a8fbfb100fbd658b10cc471a8bf0c35b
AlanG <alan.t.goff@icloud.com>
2018-09-25 13:07:19 -0400

    Started the token and player controller

---
+++ Unity Project/DnDSim/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            gameObject.transform.Translate(1,0,0);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            gameObject.transform.Translate(-1, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            gameObject.transform.Translate(0, 0, 1);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            gameObject.transform.Translate(0, 0, -1);
+        }
+    }
+}
---
+++ Unity Project/DnDSim/Assets/Scripts/TokenScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TokenScript : MonoBehaviour {
+    private Character character;//the character the token corresponds to
+    private GameObject characterGO;//character GameObject
+    private GameObject CharacterSheet;
+
+    private void Start()
+    {
+        character = transform.parent.gameObject.GetComponent<Character>();
+    }
+
+    private void OnMouseDown()//clicking on token enables and disables the character sheet
+    {
+        if (characterGO.activeInHierarchy == false)
+            characterGO.SetActive(true);
+        else
+            characterGO.SetActive(false);
+    }
+
+}
