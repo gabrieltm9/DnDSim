b47d9a35cabda6a2c8d99dafee38180c13c4a5f2
AlanG <alan.t.goff@icloud.com>
2018-09-25 14:58:59 -0400

    More setup to players and tokens

--- Unity Project/DnDSim/Assets/Scripts/PlayerController.cs
+++ Unity Project/DnDSim/Assets/Scripts/PlayerController.cs
@@ -6,21 +6,21 @@ public class PlayerController : MonoBehaviour {
 
     public void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKey(KeyCode.W))
         {
-            gameObject.transform.Translate(1,0,0);
+            gameObject.transform.Translate(0,0,1);
         }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            gameObject.transform.Translate(-1, 0, 0);
-        }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            gameObject.transform.Translate(0, 0, 1);
-        }
-        if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKey(KeyCode.S))
         {
             gameObject.transform.Translate(0, 0, -1);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.Translate(-1, 0,0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.Translate(1, 0, 0);
         }
     }
 }
